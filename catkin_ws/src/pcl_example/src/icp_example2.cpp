63a859fe7186fe7d92095f7a69ec717347bf2050
yungshansu <michael1994822@gmail.com>
2017-11-21 18:35:26 +0000

    delete task3 answer

--- catkin_ws/src/pcl_example/src/icp_example2.cpp
+++ catkin_ws/src/pcl_example/src/icp_example2.cpp
@@ -42,13 +42,16 @@ PointCloudXYZRGB::Ptr model_icp_align (new PointCloudXYZRGB);
 
 void  cloud_cb (const sensor_msgs::PointCloud2ConstPtr& input)
 {	
-	
- 	pcl::fromROSMsg (*input, *model_t1);
- 	if (model_t0->points.size() == model_t1->points.size()){
-		icp_pose_estimation (model_t0, model_t1);
-		pose_publish (model_icp_align,model_t0, model_t1);
+	//Step 1: Convert pointcloud type from Ros pointcloud2  (input) to PCL pointCLoudXYZRGB (model_t1)
+
+	//Step 2: Use function icp_pose_estimation() to register consequently moving object (model_t0, model_t1) 
+	//Step 3: Use function pose_publish() to publish moving object and aligned object pointcloud
+	if (model_t0->points.size() == model_t1->points.size()){
+
+
  	}
- 	copyPointCloud(*model_t1,*model_t0);
+	//Step 4: Update the current point (model_t0)
+
 }   
 
 void  pose_publish (PointCloudXYZRGB::Ptr cloud_icp_align,PointCloudXYZRGB::Ptr cloud_t0, PointCloudXYZRGB::Ptr cloud_t1){
@@ -84,7 +87,6 @@ void  pose_publish (PointCloudXYZRGB::Ptr cloud_icp_align,PointCloudXYZRGB::Ptr 
 	double y_error= fabs((average_cloud_t1_y-average_cloud_icp_align_y)/average_cloud_t1_y);
 	double z_error= fabs((average_cloud_t1_z-average_cloud_icp_align_z)/average_cloud_t1_z);
 	printf("x_error: %3lf percent, y_error: %3lf percent, z_error: %3lf percent\n",x_error*100,y_error*100,z_error*100);
-	///////////////////////////////////
 
 
 	cloud_t0->header.frame_id = "/camera_link";
@@ -104,6 +106,16 @@ void icp_pose_estimation (PointCloudXYZRGB::Ptr cloud_t0, PointCloudXYZRGB::Ptr 
 	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree1 (new pcl::search::KdTree<pcl::PointXYZRGB>);
 	pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree2 (new pcl::search::KdTree<pcl::PointXYZRGB>);
 	///////////Exercise : Use pcl icp function to get transform matrix///////////////////
+
+
+
+
+
+
+
+
+
+
 
 	
 	////////////////////////////////////////////////////////////////////////////////////
